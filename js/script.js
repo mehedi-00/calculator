a49c5a1a60f57105457688fa2df28c313d40d2c6
Md Mehedi Hassan <82909845+mehedi-00@users.noreply.github.com>
2022-11-09 10:54:51 +0600

    First Upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((item)=>{
+    item.onclick = ()=>{
+        if(item.id == 'clear'){
+            display.innerText = '';
+        }else if(item.id  == 'backspace'){
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0,string.length - 1);
+        }else if(display.innerText != '' && item.id == 'equal'){
+            display.innerText = eval(display.innerText);
+        }else if(display.innerText == '' && item.id == 'equal'){
+            display.innerText = 'Empty!';
+            setTimeout(()=>(display.innerText = ''),2000);
+        }else{
+            display.innerText += item.id;
+        }
+    }
+})
+const themToggleBtn = document.querySelector('.them-toggoler');
+const calculator = document.querySelector('.calculator');
+const toggleIcon = document.querySelector('.toggler-icon');
+let isDark  = true;
+themToggleBtn.onclick = ()=>{
+    calculator.classList.toggle('dark');
+    themToggleBtn.classList.toggle('active');
+    isDark = !isDark;
+}
